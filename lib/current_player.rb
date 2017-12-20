6d9b6a03bb449ce09108a92784053a9727a9792d
Zachary Wolner <zachary.wolner@gmail.com>
2017-12-20 18:55:28 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,16 +1,16 @@
 def turn_count(board)
-  turn_count = 0
+  count = 0
   board.each do |turn|
     if turn == "X" || "O"
-      turn_count += 1
+      count += 1
     end
   end
 end
 
 def current_player
-  if turn_count % 2 == 1
+  if count % 2 == 1
     puts "X"
-    if turn_count % 2 == 0
+    if count % 2 == 0
       puts "O"
     end
   end
